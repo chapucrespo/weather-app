cc62c119d7cb2a12c77742166198bac16d54060b
Marina Crespo <marina.crespo@valtech.com>
2023-09-14 14:09:12 -0300

    build basic request

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import request from "postman-request";
 
 const url =
-    "http://api.weatherstack.com/current?access_key=3a23847a32b8510cae16e7016c5c125d&query=Buenos%20Aires";
+    "http://api.weatherstack.com/current?access_key=3a23847a32b8510cae16e7016c5c125d&query=Santa%20Fe";
 
-request({ url: url }, (error, response) => {
-    console.log(response);
+request({ url: url, json: true }, (error, response) => {
+    const { body } = response;
+    const { name, country } = body.location;
+    const { temperature, humidity, weather_descriptions } = body.current;
+
+    console.log(
+        `You're currently in ${name}, ${country}. The day is ${weather_descriptions[0].toLowerCase()}, with a temperature of ${temperature} degrees, and an humidity percentage of ${humidity}%`
+    );
 });
