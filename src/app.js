960d98c3ec7fb5d7fd5eee5d14334e3f873d91c4
Marina Crespo <marina.crespo@valtech.com>
2023-09-19 13:16:12 -0300

    web-servers: partials setting

--- src/app.js
+++ src/app.js
@@ -1,13 +1,49 @@
 import express from "express";
 import path from "path";
+import hbs from "hbs";
 import * as url from "url";
 
+//Paths
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const staticUrl = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+//Express main instance
 const app = express();
+
+//Handlebars engine and views location setting
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Serve static resources
 app.use(express.static(staticUrl));
 
+app.get("", (req, res) => {
+    res.render("index", {
+        tab: "Weather app",
+        title: "Welcome to my app",
+    });
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        tab: "Help section",
+        title: "Need help?",
+        paragraph: "Contact me on github",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        tab: "About this page",
+        title: "About me",
+        paragraph: "My name's Marina and I'm a Jr. Frontend developer",
+    });
+});
+
+//static weather, returns JSON
 app.get("/weather", (req, res) => {
     res.send({
         location: "Santa Fe",
